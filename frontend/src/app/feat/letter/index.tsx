ce2ae75db6f0aacf14478fe89b3b0fc50037078a
Jonathan Yeo <jonathan@sunflower>
2025-01-08 00:26:46 +1100

    Add placeholder text for letter

--- frontend/src/app/feat/letter/index.tsx
+++ frontend/src/app/feat/letter/index.tsx
@@ -1,9 +1,22 @@
 export default function Letter() {
   return (
-    <div className={`grid grid-cols-1 shadow-xl`}>
-      <div className={`h-80 bg-gradient-to-b from-white to-slate-50`}></div>
-      <div className={`h-80 bg-gradient-to-b from-white to-slate-100`}></div>
-      <div className={`h-80 bg-gradient-to-b from-white to-slate-50`}></div>
+    <div className={`relative`}>
+      <div className={`grid grid-cols-1 shadow-xl`}>
+        <div className={`h-80 bg-gradient-to-b from-white to-stone-100`}></div>
+        <div className={`h-80 bg-gradient-to-b from-white to-stone-100`}></div>
+        <div className={`h-80 bg-gradient-to-b from-white to-stone-100`}></div>
+      </div>
+      <div className={`absolute top-0 left-0 p-20 font-sans opacity-95 w-full h-full text-lg`}>
+        <h3><b>This is a cool letter title</b></h3>
+        <br></br>
+        <p>This is a letter,</p>
+        <br></br>
+        <p>This is a letter. How many words can I fit in this letter? The quick brown fox jumped over the lazy dog. This is a paragraph of text for the letter.</p>
+        <br></br>
+        <p>How many paragraphs can I fit in this letter?</p>
+        <br></br>
+        <p><b>The alphabet is made of multiple letters.</b></p>
+      </div>
     </div>
   );
 }
