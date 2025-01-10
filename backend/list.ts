0b41c88c2b9f259497b04082a1e2372b0886692d
Damon <z5425101@ad.unsw.edu.au>
2025-01-10 15:41:49 +1100

    backend stubs

---
+++ backend/list.ts
@@ -0,0 +1,15 @@
+import data from './database.json';
+
+function getNewLetters(email: string) {
+
+}
+
+function getOldLetters(email: string) {
+
+}
+
+function getSentLetters(email: string) {
+
+}
+
+export { getNewLetters, getOldLetters, getSentLetters };
