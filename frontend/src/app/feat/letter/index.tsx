3da46b20a88e1131db7070f55da5e45c844f3a34
Jonathan <galactisphere@gmail.com>
2025-01-11 16:15:19 +1100

    Enhance letter design with improved gradients, updated text content, and added sticker element

--- frontend/src/app/feat/letter/index.tsx
+++ frontend/src/app/feat/letter/index.tsx
@@ -11,24 +11,36 @@ export default function Letter() {
   return (
     <div className="relative selection:bg-orange-100 rotate-[-0.4deg] select-text bg-white aspect-[1/1.4] w-full h-[800px] shadow-xl scale-50 md:scale-[0.85] xl:scale-100">
       <div className="flex flex-col h-full">
-        <div className="h-1/3 bg-gradient-to-b from-white to-stone-100 shadow-sm"></div>
+        <div className="h-1/3 bg-gradient-to-b from-white to-stone-50 shadow-sm border-b-stone-100 border-b"></div>
         <div className="h-1/3 bg-gradient-to-b from-white to-zinc-50 shadow-2xl"></div>
-        <div className="h-1/3 bg-gradient-to-b from-stone-100 to-white shadow-md"></div>
+        <div className="h-1/3 bg-gradient-to-b from-stone-100 to-white shadow-md border-t-stone-100 border-t"></div>
       </div>
-      <div className="absolute top-0 left-0 font-sans opacity-90 w-full h-full letter-contents p-20 text-base space-y-5 aspect-[1/1.4]">
+      <div className="absolute top-0 left-0 font-sans w-full h-full letter-contents p-20 text-base space-y-5 aspect-[1/1.4] opacity-90 select-text">
         <h3 className="font-bold">This is a cool letter title</h3>
         <p>This is a letter,</p>
         <p>
-          This is a letter. How many words can I fit in this letter? The quick
-          brown fox jumped over the lazy dog. This is a paragraph of text for
-          the letter. How many paragraphs can I fit in this letter? Too many to
-        count I think. How many paragraphs can I fit in this letter? Too many to
-        count I think.</p>
-        <p>
-          How many paragraphs can I fit in this letter? How many paragraphs can
-          I fit in this letter? Too many to count I think.
+          Here’s to the crazy ones, the misfits, the rebels, the troublemakers,
+          the round pegs in the square holes… the ones who see things
+          differently — they’re not fond of rules… You can quote them, disagree
+          with them, glorify or vilify them, but the only thing you can’t do is
+          ignore them because they change things… they push the human race
+          forward, and while some may see them as the crazy ones, we see genius,
+          because the ones who are crazy enough to think that they can change
+          the world, are the ones who do.
         </p>
-        <h3 className="font-bold">The alphabet is made of multiple letters.</h3>
+        <p>How many paragraphs can I fit in this letter?</p>
+        <p>
+          <b>The alphabet is made of multiple letters.</b>
+        </p>
+        <div className="stickers">
+          <Image
+            src="/stickers/smiley.png"
+            className="absolute drop-shadow-lg hover:scale-110 transition-transform duration-5 -rotate-12 top-36 left-3/4"
+            width={50}
+            height={50}
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
