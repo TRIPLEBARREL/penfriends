a9e6a7550bf9b5bd4051f0392578d17077db8b03
Damon <z5425101@ad.unsw.edu.au>
2025-01-13 16:12:24 +1100

    import json file changes

--- backend/inventory.ts
+++ backend/inventory.ts
@@ -1,15 +1,22 @@
-import data from './database.json';
+import data from './database.json' with { type: "json" };
+import fs from 'fs';
 
 function getPurchasedBackgrounds(email: string): string[] {
-  return [];
+  const dataObj = JSON.parse(fs.readFileSync('./database.json', {encoding: 'utf8'}));
+  const user = dataObj.filter((user) => user.email === email);
+  return user.inventory.backgrounds;
 }
 
 function getPurchasedStickers(email: string): string[] {
-  return [];
+  const dataObj = JSON.parse(fs.readFileSync('./database.json', {encoding: 'utf8'}));
+  const user = dataObj.filter((user) => user.email === email);
+  return user.inventory.stickers;
 }
 
 function showInventory(email: string): string[] {
-  return [];
+  const dataObj = JSON.parse(fs.readFileSync('./database.json', {encoding: 'utf8'}));
+  const user = dataObj.filter((user) => user.email === email);
+  return user.inventory;
 }
 
 export { getPurchasedBackgrounds, getPurchasedStickers, showInventory };
